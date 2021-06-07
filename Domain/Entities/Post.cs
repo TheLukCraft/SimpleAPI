d55072869700af4776a2aa1e3dc3165d94e7db2b
TheLukCraft <luk1997r@o2.pl>
2021-06-07 20:26:13 +0200

    added folders and basic class/interfaces in Domain

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Post : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset EventStart { get; set; }
+
+        public Post()
+        {
+
+        }
+        public Post(int id, string title, string content, DateTimeOffset eventStart)
+        {
+            (Id, Title, Content, EventStart) = (id, title, content, eventStart);
+        }
+    }
+}
